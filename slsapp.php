d1da83995f24c7054002bbdc34a33a5532367360
peder <peder@earthpeople.se>
2012-09-11 15:05:07 +0200

    new method for get_tickets

--- slsapp.php
+++ slsapp.php
@@ -1,5 +1,5 @@
 <?php
-# error_reporting(E_ALL);
+#error_reporting(E_ALL);
 
 # dirty scraping lib for springloops since they don't provide an api yet (20120910).
 # no license, just use at your own risk.
@@ -8,22 +8,24 @@
 
 # usage as standalone php class:
 # $slsapp = new Slsapp;
-# $response = $slsapp->get_milestones();
+# $milestones = $slsapp->get_milestones();
+# $tickets = $slsapp->get_tickets();
 
 # usage from codeigniter:
 # $this->load->library('slsapp');
-# this->slsapp->get_milestones();
+# $milestones = this->slsapp->get_milestones();
+# $tickets = this->slsapp->get_tickets();
 
 # known issues:
-# - class does not report back if a milestone is late, upcoming or done.
-# - cannot yet get tickets by project id, but should be straight forward to implement if needed.
+# - get_milestones does not report back if a milestone is late, upcoming or done.
 # - error handling when login fails is... not excellent.
+# - to use get_tickets, you may need to change 5051 to the integer in the location field of your browser when visiting the your tickets page in slsapp
 
 class Slsapp {
 
-	private static $base_url = 'https://XXX.slsapp.com/';
-	private static $username = 'XXX';
-	private static $password = 'XXX';
+	private static $base_url = 'https://xxx.slsapp.com/';
+	private static $username = 'xxx';
+	private static $password = 'xxx';
 	private static $tmpdir = '/tmp/';
 
 	function __construct(){
@@ -43,6 +45,7 @@ class Slsapp {
 			CURLOPT_POST => 1,
 			CURLOPT_RETURNTRANSFER => false,
 			CURLOPT_CONNECTTIMEOUT => 10,
+			#CURLOPT_VERBOSE => true,
 			CURLOPT_TIMEOUT => 10,
 			CURLOPT_COOKIEJAR => $this::$tmpdir.'slsapp.cookie',
 			CURLOPT_COOKIEFILE => $this::$tmpdir.'slsapp.cookie',
@@ -114,6 +117,31 @@ class Slsapp {
 		}	
 	}
 	
+	public function get_tickets($project_id = null, $milestone_id = null){
+		$method = 'tickets/5051?format=json';
+		$ticket = array();
+		$ttl = 18000;
+		if($response = $this->_request($this::$base_url.$method, $ttl)){
+			if($response = @json_decode($response)){
+				foreach($response->tickets as $item){
+					$ticket[$item->projectId][$item->id]->date->created = $item->created;
+					$ticket[$item->projectId][$item->id]->date->updated = $item->updated;
+					$ticket[$item->projectId][$item->id]->date->due = $item->dueDate;
+					$ticket[$item->projectId][$item->id]->assignment = $item->assignment;
+					$ticket[$item->projectId][$item->id]->title = $item->title;
+					$ticket[$item->projectId][$item->id]->description = $item->desc;
+					$ticket[$item->projectId][$item->id]->priority = $item->priority->name;
+					$ticket[$item->projectId][$item->id]->status = $item->status->name;
+					$ticket[$item->projectId][$item->id]->milestone = $item->milestone->id;
+				}
+				return $ticket;
+			}
+		}else{
+			$this->_login();
+			return $this->get_tickets();
+		}
+	}
+	
 	private function _request($url, $ttl = 86400, $force_new = 0) {
 		$cachefile = $this::$tmpdir.'slsapp_'.md5($url).'.cache';
 		$filemtime = @filemtime($cachefile); 
@@ -124,6 +152,7 @@ class Slsapp {
 				CURLOPT_RETURNTRANSFER => true,
 				CURLOPT_FOLLOWLOCATION => true,
 				CURLOPT_CONNECTTIMEOUT => 10,
+				#CURLOPT_VERBOSE => true,
 				CURLOPT_TIMEOUT => 10,
 				CURLOPT_COOKIEJAR => $this::$tmpdir.'slsapp.cookie',
 				CURLOPT_COOKIEFILE => $this::$tmpdir.'slsapp.cookie',
